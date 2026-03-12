bd4839a92041cf3c6e57061cf40b594f660650c4
tbsten <programmingcafeteria@gmail.com>
2026-03-12 19:38:26 +0900

    init from Compose Multiplatform Wizard

---
+++ webApp/src/commonMain/kotlin/main.kt
@@ -0,0 +1,6 @@
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
+import me.tbsten.sample.metro.crashbynamedfunctiontype.App
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() = ComposeViewport { App() }
